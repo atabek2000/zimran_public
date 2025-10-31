46fa0750474b994c39977d16ac3cd86dd2d462b3
atabek <nuriddin.atabek2000@gmail.com>
2025-10-31 10:40:33 +0500

    fix: logo, seo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -36,7 +36,7 @@ const closeMobileMenu = () => { showMobileMenu.value = false }
           <button
             type="button"
             @click="openRegister"
-            class=" rounded-full bg-black px-6 py-3 text-base font-medium text-concrete whitespace-nowrap hover:bg-neutral-900 active:bg-neutral-800 transition"
+            class=" rounded-full bg-black px-6 py-3 text-base font-medium text-concrete whitespace-nowrap hover:bg-black/0 hover:ring-1 hover:ring-black hover:text-black transition"
           >
             Оставить заявку
           </button>
